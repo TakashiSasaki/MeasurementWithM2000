6ef61ce727f7cbd9a34fcec8125af69e73ecabdf
Takashi SASAKI <takashi316@gmail.com>
2021-02-10 16:35:49 +0900

    remove bandwidth settings from TCP measurements

--- test.bat
+++ test.bat
@@ -48,45 +48,45 @@ iperf3.exe -d -u -r -b 10000M -t %duration% --json --get-server-output -P 10 -c 
 
 REM TCP
 timeout /nobreak %timeout%
-iperf3.exe -d -b 10000M -t %duration% --json --get-server-output -P 1 -c %server% --logfile tcp-1.log
+iperf3.exe -d  -t %duration% --json --get-server-output -P 1 -c %server% --logfile tcp-1.log
 timeout /nobreak %timeout%
-iperf3.exe -d -b 10000M -t %duration% --json --get-server-output -P 2 -c %server% --logfile tcp-2.log
+iperf3.exe -d  -t %duration% --json --get-server-output -P 2 -c %server% --logfile tcp-2.log
 timeout /nobreak %timeout%
-iperf3.exe -d -b 10000M -t %duration% --json --get-server-output -P 3 -c %server% --logfile tcp-3.log
+iperf3.exe -d  -t %duration% --json --get-server-output -P 3 -c %server% --logfile tcp-3.log
 timeout /nobreak %timeout%
-iperf3.exe -d -b 10000M -t %duration% --json --get-server-output -P 4 -c %server% --logfile tcp-4.log
+iperf3.exe -d  -t %duration% --json --get-server-output -P 4 -c %server% --logfile tcp-4.log
 timeout /nobreak %timeout%
-iperf3.exe -d -b 10000M -t %duration% --json --get-server-output -P 5 -c %server% --logfile tcp-5.log
+iperf3.exe -d  -t %duration% --json --get-server-output -P 5 -c %server% --logfile tcp-5.log
 timeout /nobreak %timeout%
-iperf3.exe -d -b 10000M -t %duration% --json --get-server-output -P 6 -c %server% --logfile tcp-6.log
+iperf3.exe -d  -t %duration% --json --get-server-output -P 6 -c %server% --logfile tcp-6.log
 timeout /nobreak %timeout%
-iperf3.exe -d -b 10000M -t %duration% --json --get-server-output -P 7 -c %server% --logfile tcp-7.log
+iperf3.exe -d  -t %duration% --json --get-server-output -P 7 -c %server% --logfile tcp-7.log
 timeout /nobreak %timeout%
-iperf3.exe -d -b 10000M -t %duration% --json --get-server-output -P 8 -c %server% --logfile tcp-8.log
+iperf3.exe -d  -t %duration% --json --get-server-output -P 8 -c %server% --logfile tcp-8.log
 timeout /nobreak %timeout%
-iperf3.exe -d -b 10000M -t %duration% --json --get-server-output -P 9 -c %server% --logfile tcp-9.log
+iperf3.exe -d  -t %duration% --json --get-server-output -P 9 -c %server% --logfile tcp-9.log
 timeout /nobreak %timeout%
-iperf3.exe -d -b 10000M -t %duration% --json --get-server-output -P 10 -c %server% --logfile tcp-10.log
+iperf3.exe -d  -t %duration% --json --get-server-output -P 10 -c %server% --logfile tcp-10.log
 
 REM TCP Reverse
 timeout /nobreak %timeout%
-iperf3.exe -d -r -b 10000M -t %duration% --json --get-server-output -P 1 -c %server% --logfile tcp-r-1.log
+iperf3.exe -d -r  -t %duration% --json --get-server-output -P 1 -c %server% --logfile tcp-r-1.log
 timeout /nobreak %timeout%
-iperf3.exe -d -r -b 10000M -t %duration% --json --get-server-output -P 2 -c %server% --logfile tcp-r-2.log
+iperf3.exe -d -r  -t %duration% --json --get-server-output -P 2 -c %server% --logfile tcp-r-2.log
 timeout /nobreak %timeout%
-iperf3.exe -d -r -b 10000M -t %duration% --json --get-server-output -P 3 -c %server% --logfile tcp-r-3.log
+iperf3.exe -d -r  -t %duration% --json --get-server-output -P 3 -c %server% --logfile tcp-r-3.log
 timeout /nobreak %timeout%
-iperf3.exe -d -r -b 10000M -t %duration% --json --get-server-output -P 4 -c %server% --logfile tcp-r-4.log
+iperf3.exe -d -r  -t %duration% --json --get-server-output -P 4 -c %server% --logfile tcp-r-4.log
 timeout /nobreak %timeout%
-iperf3.exe -d -r -b 10000M -t %duration% --json --get-server-output -P 5 -c %server% --logfile tcp-r-5.log
+iperf3.exe -d -r  -t %duration% --json --get-server-output -P 5 -c %server% --logfile tcp-r-5.log
 timeout /nobreak %timeout%
-iperf3.exe -d -r -b 10000M -t %duration% --json --get-server-output -P 6 -c %server% --logfile tcp-r-6.log
+iperf3.exe -d -r  -t %duration% --json --get-server-output -P 6 -c %server% --logfile tcp-r-6.log
 timeout /nobreak %timeout%
-iperf3.exe -d -r -b 10000M -t %duration% --json --get-server-output -P 7 -c %server% --logfile tcp-r-7.log
+iperf3.exe -d -r  -t %duration% --json --get-server-output -P 7 -c %server% --logfile tcp-r-7.log
 timeout /nobreak %timeout%
-iperf3.exe -d -r -b 10000M -t %duration% --json --get-server-output -P 8 -c %server% --logfile tcp-r-8.log
+iperf3.exe -d -r  -t %duration% --json --get-server-output -P 8 -c %server% --logfile tcp-r-8.log
 timeout /nobreak %timeout%
-iperf3.exe -d -r -b 10000M -t %duration% --json --get-server-output -P 9 -c %server% --logfile tcp-r-9.log
+iperf3.exe -d -r  -t %duration% --json --get-server-output -P 9 -c %server% --logfile tcp-r-9.log
 timeout /nobreak %timeout%
-iperf3.exe -d -r -b 10000M -t %duration% --json --get-server-output -P 10 -c %server% --logfile tcp-r-10.log
+iperf3.exe -d -r  -t %duration% --json --get-server-output -P 10 -c %server% --logfile tcp-r-10.log
 
