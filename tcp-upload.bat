3a5816e854d4fe220e126982fce92a583b9ac538
Takashi SASAKI <takashi316@gmail.com>
2021-02-12 22:40:16 +0900

    split tests into four batch files

---
+++ tcp-upload.bat
@@ -0,0 +1,45 @@
+REM TCP Download
+
+call dt.bat
+set directory=%dt%
+mkdir %dt%
+
+timeout /nobreak %waitbefore%
+set P=1
+iperf3.exe -d   -t %duration% --json --get-server-output -P %P% -c %server% --logfile %directory%/P%P%.log
+
+timeout /nobreak %waitbefore%
+set P=2
+iperf3.exe -d   -t %duration% --json --get-server-output -P %P% -c %server% --logfile %directory%/P%P%.log
+
+timeout /nobreak %waitbefore%
+set P=3
+iperf3.exe -d   -t %duration% --json --get-server-output -P %P% -c %server% --logfile %directory%/P%P%.log
+
+timeout /nobreak %waitbefore%
+set P=4
+iperf3.exe -d   -t %duration% --json --get-server-output -P %P% -c %server% --logfile %directory%/P%P%.log
+
+timeout /nobreak %waitbefore%
+set P=5
+iperf3.exe -d   -t %duration% --json --get-server-output -P %P% -c %server% --logfile %directory%/P%P%.log
+
+timeout /nobreak %waitbefore%
+set P=6
+iperf3.exe -d   -t %duration% --json --get-server-output -P %P% -c %server% --logfile %directory%/P%P%.log
+
+timeout /nobreak %waitbefore%
+set P=7
+iperf3.exe -d   -t %duration% --json --get-server-output -P %P% -c %server% --logfile %directory%/P%P%.log
+
+timeout /nobreak %waitbefore%
+set P=8
+iperf3.exe -d   -t %duration% --json --get-server-output -P %P% -c %server% --logfile %directory%/P%P%.log
+
+timeout /nobreak %waitbefore%
+set P=9
+iperf3.exe -d   -t %duration% --json --get-server-output -P %P% -c %server% --logfile %directory%/P%P%.log
+
+timeout /nobreak %waitbefore%
+set P=10
+iperf3.exe -d   -t %duration% --json --get-server-output -P %P% -c %server% --logfile %directory%/P%P%.log
