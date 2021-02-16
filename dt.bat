b25a07b267df9a59e97905649fa5e5d35bffbabd
Takashi SASAKI <takashi316@gmail.com>
2021-02-16 14:47:49 +0900

    move settings to settings.bat

--- dt.bat
+++ dt.bat
@@ -5,10 +5,4 @@ set t=%time:~0,2%%time:~3,2%%time:~6,2%
 set t=%t: =0%
 REM echo %t%
 set dt=%d%T%t%
-echo current datetime is %dt%
-
-REM iperf3 Settings
-set server=iperf.local
-set server=133.71.201.21
-set server=uchikonas.myqnapcloud.com
-set duration=30
+echo Current datetime is %dt%
---
+++ settings.bat
@@ -0,0 +1,6 @@
+REM iperf3 Settings
+set server=iperf.local
+set server=133.71.201.21
+set server=uchikonas.myqnapcloud.com
+set server=210.130.164.156
+set duration=30
