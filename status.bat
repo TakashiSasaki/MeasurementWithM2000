9f94f1c8f556a92ba3b434196d802b8a12ad802d
Takashi SASAKI <takashi316@gmail.com>
2021-02-16 14:44:16 +0900

    update session id in status.bat

--- status.bat
+++ status.bat
@@ -1,7 +1,8 @@
-set mifisess=180cb180c950c3b2852568f61731eb414cada1bc
+set mifisess=7bc6ee699aee5f0d430e31a254ca06069604507a
 set directory=status
 :loop
 call dt.bat
 curl -b mifisess=%mifisess% http://my.mifi/srv/status?_=%dt% >%directory%\%dt%.json
-timeout 10
+curl -b mifisess=%mifisess% http://my.mifi/diagnostics/ >%directory%\%dt%.html
+timeout 1
 goto loop
