8ab52e5e1effe1c733de0c300f6a9a01ab4ffc8d
Takashi SASAKI <takashi316@gmail.com>
2021-02-20 10:54:44 +0900

    HeatMap.py generates two html files

--- HeatMap.py
+++ HeatMap.py
@@ -18,16 +18,41 @@ class HeatMap():
 
 if __name__ == "__main__":
     statuses = Statuses()
-    #statuses.readFiles("status/*.*")
+    statuses.readFiles("status/*.*")
     print(len(statuses))
+
     gpxPoints = GpxPoints()
-    #gpxPoints.readFiles("gpx/*.gpx")
+    gpxPoints.readFiles("gpx/*.gpx")
     maxLat = 33.85267376174127
     minLon = 132.76945182276026
     minLat = 33.84739214519674
     maxLon = 132.77565640743407
     gpxPoints.crop(minLat, maxLat, minLon, maxLon)
     print(len(gpxPoints))
-    map = folium.Map(location=[(minLat+maxLat)/2.0, (minLon +maxLon)/2.0],zoom_start=17, 
+
+    ssHeatmapData = []
+    snrHeatmapData = []
+    for status in statuses.statuses:
+        dt,ss,snr,band  = status
+        try:
+            gpxPoint = gpxPoints.getLogByDateTime(dt)
+            lat, lon, ele, time, speed = gpxPoint
+            ssHeatmapData.append([lat, lon, ss])
+            snrHeatmapData.append([lat, lon, int(snr)])
+        except:
+            continue
+    
+    print(len(ssHeatmapData))
+    print(ssHeatmapData)
+    print(len(snrHeatmapData))
+    print(snrHeatmapData)
+    
+    ssHeatmap = folium.Map(location=[(minLat+maxLat)/2.0, (minLon +maxLon)/2.0],zoom_start=17, 
     min_lat = minLat, max_lat=maxLat, min_lon=minLon, max_lon=maxLon)
-    map.save("heatmap.html")
+    folium.plugins.HeatMap(ssHeatmapData).add_to(ssHeatmap)
+    ssHeatmap.save("ssHeatmap.html")
+
+    snrHeatmap = folium.Map(location=[(minLat+maxLat)/2.0, (minLon +maxLon)/2.0],zoom_start=17, 
+    min_lat = minLat, max_lat=maxLat, min_lon=minLon, max_lon=maxLon)
+    folium.plugins.HeatMap(snrHeatmapData).add_to(snrHeatmap)
+    snrHeatmap.save("snrHeatmap.html")
